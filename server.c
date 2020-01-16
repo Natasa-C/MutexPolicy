0ccd7b7330b63223994856f6353c2315b6c769d4
madalinaC <cirstea_madalina_ionela@yahoo.com>
2020-01-16 08:21:06 +0200

    server.c: handle mtx_open mtx_close

--- server.c
+++ server.c
@@ -2,12 +2,32 @@
 #include <zmq.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 /*
 To compile:  	cc -o server server.c -lzmq
 To run:		    ./server
 */
+
+struct Mutex {
+    unsigned int id;
+    int opened;
+    int locked;
+};
+
+struct Pair
+{
+    long pid;
+    struct Mutex mtx;
+    
+};
+
+struct Pair assignedMtxs[10]; // contains pid-mtx_id mapping in order of mutex creation
+long waitingPids[10]; // contains the processes that await the release of a mutex
+int mtxsNo = 0;
+int pidsNo = 0;
 
 int main(void)
 {
@@ -24,8 +44,129 @@ int main(void)
         zmq_recv(responder, buffer, 50, 0);
         printf("%s \n", buffer);
 
-        // Buffer should be processed in mtx array
-        
+        // process buffer
+
+        char delim[] = " ";
+        char *ptr = strtok(buffer, delim);
+        char funcName[50];
+        strcpy(funcName, ptr);
+        printf("function called: '%s'\n", funcName);
+
+        // handle mtx_open
+        if (strcmp(funcName, "mtx_open")) {
+
+            // parse params mtx_open mtx_id mtx_pid
+            ptr = strtok(NULL, delim);
+            printf("ptr: '%s'\n", *ptr);
+
+            const unsigned int base = 10;       
+            char *stringPart;
+            long  pid = strtol(ptr, &stringPart, base);
+
+            /* If the result is 0, test for an error */
+            if (pid == 0)
+            {
+                /* If a conversion error occurred, display a message and exit */
+                if (errno == EINVAL)
+                {
+                    printf("Conversion error occurred: %d\n", errno);
+                    exit(0);
+                }
+
+                /* If the value provided was out of range, display a warning message */
+                if (errno == ERANGE)
+                    printf("The value provided was out of range\n");
+            }
+
+            printf("The number(unsigned long integer) is %ld\n", pid);
+            printf("String part is |%s|", stringPart);
+
+            // body of mtx_open
+            if (mtxsNo == 0) {
+                // create new entry
+                mtxsNo += 1;
+
+                struct Mutex newMtx;
+                newMtx.id = mtxsNo;
+                newMtx.opened = 1;
+                newMtx.locked = 0;
+
+                struct Pair newPair;
+                newPair.pid = pid;
+                newPair.mtx = newMtx;
+
+                assignedMtxs[mtxsNo - 1] = newMtx;
+
+                // send mtx_id = 1 to user
+            }
+            else {
+                int mutexIsAvailable = 0;
+                for (int i = 0; i < mtxsNo; i++) {
+                    // get first mutex with opened status false
+                    if (assignedMtxs[i].mtx.opened == 0) { // mutex was closed and can be opened again
+                        assignedMtxs[i].mtx.opened = 1; // open mutex for current pid
+                        assignedMtxs[i].mtx.locked = 0; // default lock value
+                        assignedMtxs[i].pid = pid; // associates current pid with the mutex
+                        // send mtx_id = i + 1 to user
+                        mutexIsAvailable = 1;
+                        break;
+                    }
+                }
+
+                if (!mutexIsAvailable) {
+                    // create new entry
+                    mtxsNo += 1;
+
+                    struct Mutex newMtx;
+                    newMtx.id = mtxsNo;
+                    newMtx.opened = 1;
+                    newMtx.locked = 0;
+
+                    struct Pair newPair;
+                    newPair.pid = pid;
+                    newPair.mtx = newMtx;
+
+                    assignedMtxs[mtxsNo - 1] = newMtx;
+
+                    // send mtx_id = mtxs_No to user
+                }
+            }
+
+        }
+        else if (strcmp(funcName, "mtx_close")) {  // handle mtx_close
+            // parse params mtx_close mtx_id pid
+
+            // parse mtx_id
+            ptr = strtok(buffer, delim);
+            printf("ptr: %s\n", *ptr);
+
+            // parse pid
+            ptr = strtok(NULL, delim);
+            printf("ptr: %s\n", *ptr);
+
+            const unsigned int base = 10;       
+            char *stringPart;
+            long  pid = strtol(ptr, &stringPart, base);
+
+            /* If the result is 0, test for an error */
+            if (pid == 0)
+            {
+                /* If a conversion error occurred, display a message and exit */
+                if (errno == EINVAL)
+                {
+                    printf("Conversion error occurred: %d\n", errno);
+                    exit(0);
+                }
+
+                /* If the value provided was out of range, display a warning message */
+                if (errno == ERANGE)
+                    printf("The value provided was out of range\n");
+            }
+
+            printf("The number(unsigned long integer) is %ld\n", pid);
+            printf("String part is |%s|", stringPart);
+        }
+
         sleep(1); 
         zmq_send(responder, "01,2", 5, 0);
     }
